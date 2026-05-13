faeb27eef6ee907b25071b5307b7bc0c9abbfc26
mehedi <mehedi25122003@gmail.com>
2026-05-13 23:26:41 +0600

    add coalesce so data not deleted

--- src/server.ts
+++ src/server.ts
@@ -40,54 +40,78 @@ const initdb = async () => {
   }
 };
 initdb();
-app.get("/api/users", async(req: Request, res: Response) => {
+app.get("/api/users", async (req: Request, res: Response) => {
   const result = await pool.query(`
-    SELECT * FROM users`)
+    SELECT * FROM users`);
 
-    res.status(200).json({success:true,
-      message: "data exists bro",
-      data: result.rows
-    })
-
+  res
+    .status(200)
+    .json({ success: true, message: "data exists bro", data: result.rows });
 });
 
-app.get("/api/users/:id", async(req:Request,res:Response)=>{
-  const {id} = req.params;
+app.get("/api/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
   try {
-    const result = await pool.query(`SELECT * FROM users WHERE id=$1`,[id])
-    if(result.rows.length === 0){
-res.status(500).json({
-    success:false,
-    message: "not found this data man",
+    const result = await pool.query(`SELECT * FROM users WHERE id=$1`, [id]);
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "not found this data man",
+        data: {},
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "got this bro",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+});
+
+app.post("/api/users", async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
+    const result = await pool.query(
+      `INSERT INTO users(name,email,password) VALUES($1,$2,$3) RETURNING *`,
+      [name, email, password],
+    );
+    res.status(201).json({ data: result.rows[0] });
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ success: false, message: error.message, error: error });
+  }
+});
+
+app.put("/api/users/:id", async (req: Request,res:Response)=>{
+  const {id} = req.params;
+  const {name,email,password,is_active} = req.body;
+  const result = await pool.query(`UPDATE users SET name=COALESCE($1,name), email=COALESCE($2,email), password=COALESCE($3,password), is_active=COALESCE($4,is_active) WHERE id=$5 RETURNING *`,[name,email,password,is_active,id])
+
+try {
+  if(result.rows.length === 0){
+    res.status(404).json({success:true,
+    message:"No data find here man",
     data: {}
   })
   }
-  res.status(200).json({
-    success:true,
-    message: "got this bro",
+  res.status(200).json({success:true,
+    message:"Data updated successfully",
     data: result.rows[0]
   })
-
-  
-  } catch (error:any) {
-    res.status(500).json({
-    success:false,
-    message: error.message,
-    error: error
+} catch (error) {
+  res.status(404).json({success:false,
+    message:"failed",
+    data: {}
   })
-  }
-  
+}
 })
-
-app.post("/api/users", async (req:Request, res:Response) => {
-  try {
-   const {name,email,password} = req.body;
-   const result = await pool.query(`INSERT INTO users(name,email,password) VALUES($1,$2,$3) RETURNING *`,[name,email,password])
-   res.status(201).json({data:result.rows[0]})
-  } catch (error:any) {
-    res.status(500).json({ success:false,message: error.message,error:error });
-  }
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
