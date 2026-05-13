8c48ffcd59c8dad37277ae4f1d8a637eb6bda5c5
Mehedi <mehedi25122003@gmail.com>
2026-05-13 18:53:57 +0600

    server created

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
-import express from 'express'
-const app = express()
+import express, { type Application, type Request, type Response } from 'express'
+const app:Application = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req:Request, res:Response) => {
+//   res.send('Hello World!')
+res.status(200).json({message: "hello world"})
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
