7e55e59503253be5f47684a5f3dded3a093a1048
mehediScriptDev <mehedi25122003@gmail.com>
2026-05-28 21:55:39 +0600

    token generation done

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { error } from "node:console";
 import { pool } from "../../db";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import config from "../../config";
 
 const loginUserIntoDb =  async(payload:{email:string,password:string})=>{
 const {email,password} = payload;
@@ -12,7 +14,26 @@ const userData = await pool.query(`
     if(userData.rows.length ===0){
         throw new Error("invalid credentials")
     }
-    const user = userData.rows[0];
+     const user = userData.rows[0];
+
+     const matchedPassword = await bcrypt.compare(password,user.password);
+    if(!matchedPassword){
+        throw new Error("invalid credentials")
+    }
+    
+    const jwtPayload = {
+        "id": user.id,
+        "email": user.email,
+        "name": user.name,
+        "is_active": user.is_active
+
+    }
+
+    const token = jwt.sign(jwtPayload,config.secret as string,{expiresIn:"1d"})
+
+return token;
+
+   
 }
 
 
